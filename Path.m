77186ff22e99c9a65c3477b011186a1fd12744ea
Martin <martin.koch123@posteo.de>
2021-02-08 21:05:04 +0100

    init

---
+++ Path.m
@@ -0,0 +1,322 @@
+classdef Path
+    
+    properties
+        extension_
+        stem_
+        parent_
+        root_
+    end
+    
+    properties (Constant, Hidden)
+        FILE_SEPARATOR_REGEX = regexptranslate("escape", filesep);
+    end
+    
+    methods
+        function obj = Path(args)
+            arguments (Repeating)
+                args (1, :) string {mustBeNonmissing};
+            end
+            
+            % Default constructor
+            if isempty(args)
+                obj = Path(".");
+                return
+            end
+
+            % Convert to string vector.
+            args = [args{:}];
+            
+            % Empty constructor
+            if isempty(args)
+                obj = Path.empty(1, 0);
+                return
+            end
+            
+            % Resolve path separators.
+            args = args.join(pathsep).split(pathsep);          
+            pathCount = length(args);
+            
+            obj(pathCount) = obj;
+            fs = Path.FILE_SEPARATOR_REGEX;
+            
+            for i = 1 : pathCount
+                args(i) = Path.clean(args(i));
+                
+                % Extract parent directory and name.
+                match = regexp(args(i), "^(?<parent>.*?)(?<name>[^"+fs+"]+)$", "names");
+                
+                % Extract stem and extension from name.
+                if match.name == ".." || match.name == "."
+                    stem = match.name;
+                    extension = "";
+                else
+                    match2 = regexp(match.name, "^(?<stem>.*?)(?<extension>(\.[^\.]*|))$", "names");
+                    extension = match2.extension;
+                    stem = match2.stem;
+                end
+                
+                % Extract root from parent directory.
+                rootMatch = regexp(match.parent, "^(?<root>(\\\\[^\\]+|\w:|)\\?)(?<rest>.*)", "names", "emptymatch");
+                
+                obj(i).parent_ = rootMatch.rest;
+                obj(i).stem_ = stem;
+                obj(i).extension_ = extension;
+                obj(i).root_ = rootMatch.root;
+            end
+        end
+        
+        function disp(objects)            
+            fprintf("  %i×%i Path array\n\n", size(objects, 1), size(objects, 2));            
+            if isempty(objects)
+                return; end
+            for obj = objects
+                fprintf("     Path(""%s"")\n", obj.string);
+            end
+            fprintf("\n");
+        end
+        
+        
+        %% Conversion
+        function result = string(objects)
+            result = [objects.root_] + [objects.parent_] + [objects.stem_] + [objects.extension_];
+        end
+        
+        function result = char(obj)
+            arguments
+                obj (1, 1)
+            end
+            result = char(obj.string);
+        end
+        
+        function result = charCell(objects)
+            result = arrayfun(@char, objects, 'UniformOutput', false);
+        end
+        
+        %% Properties        
+        function result = name(objects)
+            result = Path([objects.stem_] + [objects.extension_]);
+        end
+        
+        function result = extension(objects)
+            result = [objects.extension_];
+        end
+        
+        function result = stem(objects)
+            result = [objects.stem_];
+        end
+        
+        function result = parent(objects)
+            result = Path([objects.root_] + [objects.parent_]);
+        end
+        
+        function result = isRelative(objects)
+            
+        end
+        
+        %% Manipulation
+        function result = append(objects, appendage)
+            arguments
+                objects(1, :)
+            end
+            arguments (Repeating)
+                appendage
+            end
+            
+            appendagePath = Path(appendage{:});
+            
+            if isempty(objects) || isempty(appendagePath)
+                result = objects;
+                return 
+            elseif isscalar(objects) || isscalar(appendagePath) || length(objects) == length(appendagePath)
+                result = Path(objects.string + "/" + appendagePath.string);
+            else
+                error("Path:append:LengthMismatch", "Length of object array, %i, and length of appendage array, %i, must either match or one of them must be scalar.", length(objects), length(appendage));
+            end
+        end
+        
+        function result = mrdivide(objects, appendage)
+            result = objects.append(appendage);
+        end
+        
+        function result = mldivide(objects, appendage)
+            result = objects.append(appendage);
+        end
+        
+        %% Filter
+        function result = where(objects, filters)
+            arguments
+                objects
+                filters.Name (1, :) string = missing;
+                filters.Stem(1, :) string = missing;
+                filters.Extension (1, :) string = missing;
+                filters.Parent(1, :) string = missing;
+                filters.NameNot (1, :) string = missing;
+                filters.StemNot (1, :) string = missing;
+                filters.ExtensionNot (1, :) string = missing;
+                filters.ParentNot (1, :) string = missing;
+            end
+            
+            keep = true(size(objects));
+            for i = 1 : length(objects)
+                if ~ismissing(filters.Name)
+                    keep(i) = keep(i) & any(objects(i).name.string == filters.Name); end
+                if ~ismissing(filters.Stem)
+                    keep(i) = keep(i) & any(objects(i).stem == filters.Stem); end
+                if ~ismissing(filters.Extension)
+                    keep(i) = keep(i) & any(objects(i).extension == filters.Extension); end
+                if ~ismissing(filters.Parent)
+                    keep(i) = keep(i) & any(objects(i).parent.string == filters.Parent); end
+                if ~ismissing(filters.NameNot)
+                    keep(i) = keep(i) & all(objects(i).name.string ~= filters.NameNot); end
+                if ~ismissing(filters.StemNot)
+                    keep(i) = keep(i) & all(objects(i).stem ~= filters.StemNot); end
+                if ~ismissing(filters.ExtensionNot)
+                    keep(i) = keep(i) & all(objects(i).extension ~= filters.ExtensionNot); end                
+                if ~ismissing(filters.ParentNot)
+                    keep(i) = keep(i) & all(objects(i).parent.string ~= filters.ParentNot); end       
+            end
+            result = objects(keep);
+        end
+        
+        %% File system interaction        
+        function result = exists(objects)
+            result = objects.fileExists & objects.folderExists;
+        end
+        
+        function result = fileExists(objects)
+            result = arrayfun(@(obj) isfile(obj.string), objects);
+        end
+        
+        function result = folderExists(objects)
+            result = arrayfun(@(obj) isfolder(obj.string), objects);
+        end            
+        
+        function mkdir(objects)
+            for obj = objects
+                if obj.folderExists
+                    return;
+                end
+                try
+                    mkdir(obj.string);
+                catch exception
+                    if startsWith(string(exception.identifier), 'MATLAB:MKDIR')
+                        error(exception.identifier, "Error while creating folder ""%s"".\n%s", obj, exception.message); end
+                end
+            end
+        end
+        
+        function writeEmptyFile(objects)
+            for obj = objects
+                obj.parent.mkdir;
+                fileId = fopen(obj.string, 'w');
+                fclose(fileId);
+            end
+        end
+        
+        %% Save and load        
+        function save(obj, variables)
+            arguments
+                obj (1, 1)
+            end
+            arguments (Repeating)
+                variables (1, 1) string {mustBeValidVariableName}
+            end
+            if isempty(variables)
+                error("Path:load:MissingArgument", "Not enough inputs arguments.");
+            end
+            for variable = [variables{:}]
+                saveStruct.(variable) = evalin("caller", variable);
+            end
+            obj.parent.mkdir;
+            save(obj.string, "-struct", "saveStruct");
+        end
+        
+        function varargout = load(obj, variables)
+            arguments
+                obj (1, 1)
+            end
+            arguments (Repeating)
+                variables (1, 1) string {mustBeValidVariableName}
+            end
+
+            if nargout ~= length(variables)
+                error("Path:load:InputOutputMismatch", "The number of outputs, %i, must match the number of variables to load, %i.", nargout, length(variables)); end
+            data = load(obj.string, variables{:});
+            varargout = {};
+            for variable = string(variables)
+                if ~isfield(data, variable)
+                    error("Path:load:VariableNotFound", "Variable ""%s"" not found in file ""%s"".", variable, obj); end
+                varargout{end+1} = data.(variable);
+            end
+        end
+    end
+    
+    methods (Static)
+        function result = ofMatlabElement(elements)
+            arguments
+                elements (1, :) string {mustBeNonmissing}
+            end
+            result = Path.empty;
+            for element = elements
+                path = string(which(element));
+                
+                % If the queried element happens to have the name of a
+                % variable in this function, temporarily rename that
+                % variable.
+                if path == "variable"
+                    temp = eval(element);
+                    clearvars(element);
+                    path = string(which(element));
+                    eval(element + " = temp");
+                end
+                
+                if path.startsWith("built")
+                    
+                    % Remove "build in" and brackets.
+                    path = regexprep(path, ["^[^\(]*\(", "\)$"], "");
+                elseif path == ""
+                    error("Path:ofMatlabElement:NotFound", "Element ""%s"" is not on the search path.", element);
+                end
+                result(end+1) = Path(path);
+            end
+        end
+        
+        function result = ofCaller
+            stack = dbstack;
+            if length(stack) == 1
+                error("Path:ofCaller:NoCaller", "This method was not called from another file."); end
+            callingFile = stack(2).file;
+            result = Path.ofMatlabElement(callingFile);
+        end 
+    end
+    
+    methods (Static, Access = private)
+        function s = clean(s)
+            fs = Path.FILE_SEPARATOR_REGEX;
+            
+            s = s.strip;
+            
+            % Replace / and \ with correct separator.
+            s = s.replace(["\", "/"], fs);
+            
+            % Remove repeating separators.
+            s = regexprep(s, fs + "*", fs);
+            
+            % Remove leading and trailing separators.
+            s = regexprep(s, ["^"+fs+"+", fs+"+$"], "");
+            
+            % Remove current-directory-dots.
+            s = regexprep(s, ["(?<=(^|"+fs+"))(\."+fs+")", "("+fs+"\.)$"], "");
+            
+            % Resolve folder-up-dots.
+            expression = "("+fs+"|^)[^"+fs+":]+(?<!\.\.)"+fs+"\.\."; % Folder name folled by folder-up dots.
+            while ~isempty(regexp(s, expression, 'once'))
+                s = regexprep(s, expression, "");
+            end
+            
+            if s == ""
+                s = ".";
+            end
+        end
+    end
+end
---
+++ PathTest.m
@@ -0,0 +1,307 @@
+classdef PathTest < matlab.unittest.TestCase
+    
+    properties (Constant)
+        testFolder = Path.ofMatlabElement("PathTest").parent / "test";
+    end
+    
+    methods 
+        function assertFileExists(obj, files)
+            for file = files
+                obj.assertTrue(isfile(string(file)));
+            end
+        end
+        
+        function assertFileDoesNotExists(obj, files)
+            for file = files
+                obj.assertFalse(isfile(string(file)))
+            end
+        end
+        
+        function assertFolderExists(obj, folders)
+            for folder = folders
+                obj.assertTrue(isfolder(string(folder)));
+            end
+        end
+        
+        function assertFolderDoesNotExist(obj, folders)
+            for folder = folders
+                obj.assertFalse(isfolder(string(folder)));
+            end
+        end
+    end
+    
+    methods(TestMethodTeardown)
+        function removeTestFolder(testCase)
+            if testCase.testFolder.folderExists
+                rmdir(testCase.testFolder.string, "s");
+            end
+        end
+    end
+        
+    
+    methods (Test)
+        
+        %% Constructor
+        function constructWithStringVector(obj)
+            actual = Path(["one", "two"]);
+            expected = Path(["one", "two"]);
+            obj.assertEqual(actual, expected);
+        end
+        
+        function constructWithChars(obj)
+            obj.assertEqual(Path("test"), Path('test'))
+        end
+        
+        function constructWithCharCell(obj)
+            actual = Path({'one', 'two'});
+            expected = Path(["one", "two"]);
+            obj.assertEqual(actual, expected);
+        end
+        
+        function constructWithStringCell(obj)
+            actual = Path({"one", "two"});
+            expected = Path(["one", "two"]);
+            obj.assertEqual(actual, expected);
+        end
+        
+        function constructWithPathSeparator(obj)
+            obj.assertEqual(Path("one; two"), Path(["one", "two"]));
+            obj.assertEqual(Path(" ; "), Path([".", "."]));
+        end
+        
+        function constructDefault(obj)
+            obj.assertEqual(Path().string, ".");
+        end
+        
+        function constructEmpty(obj)
+            obj.assertEmpty(Path(string.empty));
+            obj.assertEmpty(Path({}));
+        end
+        
+        function constructWithMultipleArguments(obj)
+            actual = Path('a', "b; c", {'d', "e; f"}, ["g", "h"]);
+            expected = Path(["a" "b" "c" "d" "e" "f" "g", "h"]);
+            obj.assertEqual(actual, expected);
+        end
+        
+        %% Conversion
+        function string(obj)
+            obj.assertEqual(Path(["one", "two"]).string, ["one", "two"]);
+        end
+        
+        function char(obj)
+            obj.assertEqual('test', Path("test").char);
+        end
+        
+        function charCell(obj)
+            obj.assertEqual(Path("one").charCell, {'one'});
+            obj.assertEqual(Path(["one", "two"]).charCell, {'one', 'two'});
+        end
+        
+        %% Clean
+        function assertStripsWhitespace(obj)
+            obj.assertEqual("test", Path(sprintf("\n \ttest  \r")).string);
+        end
+        
+        function assertRemovesRepeatingSeparators(obj)
+            s = filesep;
+            actual = Path("one" + s + s + s + "two" + s + s + "three").string;
+            expected = adjustSeparators("one/two/three");
+            obj.assertEqual(actual, expected);
+        end
+        
+        function assertRemovesOuterSeparators(obj)
+            s = filesep;
+            actual = Path([s s 'one/two/three' s]).string;
+            expected = adjustSeparators("one/two/three");
+            obj.assertEqual(actual, expected);
+        end
+        
+        function assertRemovesCurrentDirDots(obj)
+            actual = Path("\.\.\one\.\two.three\.\.four\.\.\").string;
+            expected = adjustSeparators("one\two.three\.four");
+            obj.assertEqual(actual, expected);
+        end
+        
+        function assertReplacesSeparatorVariations(obj)
+            actual = Path("one/two\three").string;
+            expected = adjustSeparators("one/two/three");
+            obj.assertEqual(actual, expected);
+        end
+        
+        function assertResolvesParentDirDots(obj)
+            actual = Path("one/two/three/../../four");
+            expected = Path("one/four");
+            obj.assertEqual(actual, expected);
+        end
+        
+        %% Parts
+        function name(obj)
+            obj.assertEqual(Path("C:/one/two/three.ext").name.string, "three.ext");
+            obj.assertEqual(Path("one.two.three.ext").name.string, "one.two.three.ext");
+            obj.assertEqual(Path("one").name.string, "one");
+            obj.assertEqual(Path("..").name.string, "..");
+            obj.assertEqual(Path(".").name.string, ".");
+        end
+        
+        function stem(obj)
+            obj.assertEqual(Path("C:/one/two/three.ext").stem, "three");
+            obj.assertEqual(Path("one.two.three.ext").stem, "one.two.three");
+            obj.assertEqual(Path("one").stem, "one");
+            obj.assertEqual(Path("..").stem, "..");
+            obj.assertEqual(Path(".").stem, ".");
+        end
+        
+        function extension(obj)
+            obj.assertEqual(Path("C:/one/two/three.ext").extension, ".ext");
+            obj.assertEqual(Path("one.two.three.ext").extension, ".ext");
+            obj.assertEqual(Path("one.").extension, ".");
+            obj.assertEqual(Path("one").extension, "");
+            obj.assertEqual(Path("..").extension, "");
+            obj.assertEqual(Path(".").extension, "");
+        end
+        
+        function parent(obj)
+            obj.assertEqual(Path("C:/one/two/three.ext").parent, Path("C:/one/two"));
+            obj.assertEqual(Path("../../one/three.ext").parent, Path("../../one"));
+            obj.assertEqual(Path("one").parent, Path("."));
+            obj.assertEqual(Path("..").parent, Path("."));
+            obj.assertEqual(Path(".").parent, Path("."));
+        end
+        
+        %% Manipulation
+        function append(obj)
+            obj.assertEqual(Path("one").append(""), Path("one"));
+            obj.assertEqual(Path("one").append(["one", "two"]), Path("one/one", "one/two"));
+            obj.assertEqual(Path("one", "two").append("one"), Path("one/one", "two/one"));
+            obj.assertEmpty(Path.empty.append("one"), Path);
+            obj.assertEqual(Path("one").append(strings(0)), Path("one"));
+            obj.assertError(@() Path("one", "two", "three").append(["one", "two"]), "Path:append:LengthMismatch");
+            obj.assertEqual(Path("a").append("b", 'c', {'d', "e; f"}), Path("a/b", "a/c", "a/d", "a/e", "a/f"));
+        end
+        
+        function mrdivide(obj)
+            obj.assertEqual(Path("one") / "two", Path("one/two"));
+        end
+        
+        function mldivide(obj)
+            obj.assertEqual(Path("one") \ "two", Path("one/two"));
+        end
+        
+        
+        %% Filter
+        function where(obj)
+            paths = Path([
+                "C:\one\two.txt"        % 1
+                "two.txt"               % 2
+                "one\two.csv"           % 3
+                "C:\three\four.txt"     % 4
+                "three.txt"             % 5
+                "one\three.dat"         % 6
+                "five"                  % 7
+                ".."                    % 8
+                ]);
+            obj.assertEqual(paths([1, 2, 7]), paths.where("Name", ["two.txt", "five", "six"]));
+            obj.assertEqual(paths([3, 4, 5, 7]), paths.where("NameNot", ["two.txt", "three.dat", "six", ".."]));
+            
+            obj.assertEqual(paths([1, 2, 3, 4, 8]), paths.where("Stem", ["one", "two", "four", ".."]));
+            obj.assertEqual(paths([4, 7, 8]), paths.where("StemNot", ["two", "three", "six"]));
+            
+            obj.assertEqual(paths([1, 2, 4, 5, 7, 8]), paths.where("Extension", [".txt", ".mat", ""]));
+            obj.assertEqual(paths([3, 6]), paths.where("ExtensionNot", [".txt", ".mat", ""]));
+            
+            obj.assertEqual(paths([1, 3, 6]), paths.where("Parent", ["one", "C:\one", "D:\"]));
+            obj.assertEqual(paths([1, 4]), paths.where("ParentNot", ["one", ".", "D:\"]));
+            
+            obj.assertEqual(paths([2]), paths.where( ...
+                "NameNot", ["six", "three.txt"], ...
+                "Extension", [".txt", ".mat"], ...
+                "StemNot", ["four", "five"], ...
+                "ParentNot", ["C:\one", "D:\"] ...
+            ));
+        end
+        
+        %% File system interaction
+        function mkdir(obj)
+            obj.testFolder.append(["a", "b/a"]).mkdir;
+            obj.assertFolderExists(obj.testFolder / "a; b/a");
+        end
+        
+        function writeEmptyFile(obj)
+            obj.testFolder.append("a.b; c/d").writeEmptyFile;
+            obj.assertFileExists(obj.testFolder / "a.b; c/d");
+        end
+        
+        function fileExistsAndFolderExists(obj)
+            paths = obj.testFolder / "a.b; c/d";
+            obj.assertFalse(any(paths.fileExists));
+            paths.writeEmptyFile;
+            obj.assertFalse(any(paths.folderExists));
+            obj.assertTrue(all(paths.fileExists));   
+            
+            delete(paths(1).string, paths(2).string);
+            
+            obj.assertFalse(any(paths.folderExists));
+            paths.mkdir;
+            obj.assertFalse(any(paths.fileExists));
+            obj.assertTrue(all(paths.folderExists));
+        end
+        
+        %% Matlab files
+        function ofMatlabElement(obj)
+            actual = Path.ofMatlabElement(["mean", "PathTest"]).string;
+            expected = string({which("mean") which("PathTest")});
+            obj.assertEqual(actual, expected);
+            obj.assertError(@() Path.ofMatlabElement("npofas&/"), "Path:ofMatlabElement:NotFound");
+        end
+        
+        function ofCaller(obj)
+            obj.assertEqual(Path.ofCaller, Path(which("PathTest")));
+        end
+        
+        %% Save and load
+        function save(obj)
+            a = 1;
+            b = "test";
+            file = obj.testFolder / "data.mat";
+            file.save("a", "b");
+            clearvars("a", "b");
+            load(file.string, "a", "b");
+            obj.assertEqual(a, 1);
+            obj.assertEqual(b, "test"); 
+        end
+        
+        function load(obj)
+            a = 1;
+            b = "test";
+            file = obj.testFolder / "data.mat";
+            obj.testFolder.mkdir;
+            save(file.string, "a", "b");
+            clearvars("a", "b");
+            [a, b] = file.load("a", "b");
+            obj.assertEqual(a, 1);
+            obj.assertEqual(b, "test"); 
+            
+            raisedError = false;
+            try 
+                a = file.load("a", "b");
+            catch exception
+                obj.assertEqual(string(exception.identifier), "Path:load:InputOutputMismatch");
+                raisedError = true;
+            end
+            obj.assertTrue(raisedError);
+            raisedError = false;
+            try
+                c = file.load("c");
+            catch exception
+                obj.assertEqual(string(exception.identifier), "Path:load:VariableNotFound");
+                raisedError = true;
+            end
+            obj.assertTrue(raisedError);
+        end
+    end
+end
+
+function s = adjustSeparators(s)
+    s = s.replace(["/", "\"], filesep);
+end
